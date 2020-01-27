0f970b46ddcb3dbbb4c1a7b8ac35fa91bd30e537
niftynei <niftynei@gmail.com>
2020-01-27 19:23:07 +0000

    no idea, some more changes?

--- airdrop.py
+++ airdrop.py
@@ -49,38 +49,49 @@ def connect_to(rpc, nodes):
         returns the count of successful connections
     """
     count = 0
+    attempts = 0
     for n in nodes:
+        attempts += 1
         count = connect_addr_inner(rpc, n, count)
-        # for now, we limit ourselves to 10 connects
-        print(count)
-        if count == 10:
-            return count
+        print("connected {} of {}/{}".format(count, attempts, len(nodes)))
     return count
 
 
 def connect_addr_inner(rpc, n, count):
     for a in n['addresses']:
-        if a['type'] == 'ipv4':
+        if a['type'] in ['ipv4', 'ipv6', 'torv3', 'torv2']:
+            # some nodes have a poorly configured ipv6 of '::'
+            if a['address'] in ['0.0.0.0', '127.0.0.1', '::']:
+                print('skipping null address')
+                continue
             fut = executor.submit(rpc.connect, n['nodeid'], a['address'], a['port'])
             try:
                 # We time out after 3s
-                fut.result(5)
+                fut.result(3)
                 return count + 1
             except (futures.TimeoutError, RpcError) as e:
-                print(e)
+                print("errror encountered! {}".format(e))
                 continue
+    print("unable to connect to {}".format(n['nodeid']))
     return count
 
 
 def fund_connected(rpc):
+    amount_sat = 16000000
     eligible_peers = [p for p in rpc.listpeers()['peers'] if len(p['channels']) == 0]
+    count = 0
     for p in eligible_peers:
-        fut = executor.submit(rpc.fundchannel, p['id'], 1600000, minconf=0)
+        # break early, at least until i've solved the borking error
+        if count == 25:
+            return count
+        fut = executor.submit(rpc.fundchannel, p['id'], amount_sat, minconf=0, push_msat=(amount_sat // 2) * 1000)
         try:
             fut.result(5)
             print("funded channel with {}!!".format(p['id']))
+            count += 1
         except (futures.TimeoutError, RpcError) as e:
             print("error!! {}".format(e))
+    return count
 
 
 def main(connect, fund):
